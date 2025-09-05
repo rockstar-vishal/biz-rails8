45360d96f87957c44fe8afa0962fb75e8421f523
RV <rock1990vishal@gmail.com>
2025-09-05 19:31:16 +0530

    adding association

--- app/models/company.rb
+++ app/models/company.rb
@@ -7,5 +7,6 @@ class Company < ApplicationRecord
 		length: 3
 	}
 	has_many :projects
+	has_many :users
 	validates :domain, :project_limit, :unit_limit, :allowed_configs, :allowed_banks, :allowed_cost_maps, :max_registration_charges, :default_sdr_percent, :default_registration_percent, :default_legal_charges, :allowed_statuses, presence: true
 end
